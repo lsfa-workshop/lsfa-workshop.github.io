f87130e432b4999ac36f33b98e91a0495328fe8f
David Perera <perera.dap@gmail.com>
2026-01-07 22:41:55 -0300

    first draft of the 2026 version of the lfsa website

---
+++ 2026/js/main.js
@@ -0,0 +1,98 @@
+(function($) {
+	"use strict"
+
+	// Scrollspy
+	$('body').scrollspy({
+		target: '#nav',
+		offset: $(window).height() / 2
+	});
+
+	// Mobile nav toggle
+	$('.navbar-toggle').on('click',function() {
+		$('.main-nav').toggleClass('open');
+	});
+
+	// Fixed nav
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+		if (wScroll > 50)  {$('#header').addClass('fixed-navbar');$('#header').removeClass('transparent-navbar')} else {$('#header').removeClass('fixed-navbar');$('#header').addClass('transparent-navbar')};
+	});
+
+	// Smooth scroll
+	$(".main-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 800);
+	});
+
+	// Section title animation
+	$('.section-title').each(function() {
+		var $this = $(this);
+		$this.find('.title > span').each(function(i) {
+			var $span = $(this);
+			var animated = new Waypoint({
+				element: $this,
+				handler: function()
+				{
+					setTimeout(function(){
+						$span.addClass('appear')
+					}, i*250);
+					this.destroy();
+				},
+				offset: '95%'
+			});
+		});
+	});
+
+       // Fix long speaker names
+	$('.speaker .speaker-content>h2+h2').prev().css({"margin": "0px 0px 0px"});
+        $('.speaker .speaker-contentg>h2+h2').prev().css({"margin": "0px 0px 0px"});
+
+	// Galery Owl
+	$('#galery-owl').owlCarousel({
+		items:1,
+		loop:true,
+		margin:0,
+		dots : false,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		autoplay : true,
+		autoplaySpeed :500,
+		navSpeed :500,
+		responsive : {
+	    0 : {
+	       stagePadding : 0,
+	    },
+	    768 : {
+	        stagePadding : 120,
+	    }
+		}
+	});
+
+	// Parallax Background
+	$.stellar({
+		responsive: true
+	});
+
+	// CountTo
+	$('.counter').each(function() {
+		var $this = $(this);
+		var counter = new Waypoint({
+			element: $this,
+			handler: function()
+			{
+				$this.countTo();
+			},
+			offset: '95%'
+		});
+	});
+    $(".modal").on('shown.bs.modal', function () {
+        $(this).find("button").focus();
+    });
+      /* $(document).ready(function() {
+           $('#myModal').modal('show');
+        });*/
+
+})(jQuery);
